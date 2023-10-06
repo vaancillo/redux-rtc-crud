abed4bc5b4048793d317c05330ef6b3a0984273a
vancillo <yaredbane@gmail.com>
2023-10-06 00:30:34 -0600

    slices

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { useSelector } from 'react-redux'
+import TaskForm from './components/TaskForm'
+import TaskList from './components/TaskList'
 
 function App() {
+  const taskState = useSelector(state => state.tasks)
+  console.log(taskState)
 
   return (
     <div className='App'>
       <h1>hello world</h1>
+      <TaskForm />
+      <TaskList />
     </div>
   )
 }
--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit'
 import taskReducer from '../features/tasks/taskSlice'
 
 export const store = configureStore({
-    task: taskReducer
+    reducer: {
+        tasks: taskReducer
+    }
 })
 
